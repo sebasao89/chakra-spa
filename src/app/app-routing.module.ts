43fd10d50399d7fc65798a368ef98c07a33e77c7
Sebasao <sebas.ao@outlook.com>
2023-03-07 21:52:04 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListClientComponent } from './components/list-client/list-client.component';
+import { CreateClientComponent } from './components/create-client/create-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/listClient', pathMatch: 'full'},
+  {path: 'listClient', component: ListClientComponent},
+  {path: 'createClient', component: CreateClientComponent},
+  {path: '**', redirectTo: '/listClient', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/list-client/list-client.component.ts
+++ src/app/components/list-client/list-client.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-client.component.scss']
 })
 export class ListClientComponent {
-
+  
+  
+  
 }
